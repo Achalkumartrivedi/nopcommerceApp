4f4689ca2b1327f097a2beecc5d113ee06b597a6
Achal <achal.tecocraft@gmail.com>
2022-11-05 23:20:37 +0530

    Commit HTML report - conftest.py

--- testCases/test_Login.py
+++ testCases/test_Login.py
@@ -1,7 +1,7 @@
 import pytest
 from selenium import webdriver
 from pageObjects.LoginPage import Login
-from testCases.conftest import chrome_driver_init
+from testCases.conftest import setup
 import chromedriver_autoinstaller
 from utilities.readProperties import ReadConfig
 from utilities.customeLogger import LogGenclass
@@ -13,42 +13,38 @@ class Test_001_Login:
     baseurl = ReadConfig.getBaseURL()
     username = ReadConfig.getUsername()
     password =  ReadConfig.getPassword()
-    screenshotfolder = ReadConfig.getDirectory()
-   #ss_login = ReadConfig.getTest1screenshot()
-    #ss_loginpage = "../Screenshots/Login.png"
-    #ss_dash = "..\\Screenshots\\" + "dashboard.png"
-
+    #screenshotfolder = ReadConfig.getDirectory() for common folder
 
     logger = LogGenclass.loggenmethod() #call loggenmethod of LEggenClass
 
 
 
-    def test_homePageTitle(self, chrome_driver_init):
+    def test_homePageTitle(self, setup):
 
-        self.driver = chrome_driver_init
+        self.driver = setup
         self.logger.info("***** START Test 01: Login Page title verification *****")
         self.driver.get(self.baseurl)
         act_title = self.driver.title
 
         if act_title == 'Your store. Login':
-            self.logger.info("***** Test 1 :Login page Title is Correct *****")
-            print("\nTest 1: Login page title is correct\n")
+            self.logger.info("***** Test 01 PASS: Login page Title is Correct *****")
+            print("\nTest 01: Login page title is correct\n")
             self.driver.close()
             assert True
         else:
-            self.driver.save_screenshot(self.screenshotfolder+ '/Login.png')
-            self.logger.info("***** Test 1 :Login page Title is Incorrect *****")
+            self.driver.save_screenshot(self.ReadConfig.getLoginscreenshot()+ '/Login.png')
+            self.logger.info("***** Test 01 FAIL: Login page Title is Incorrect *****")
             print("LoginPage Title is incorrect")
             self.driver.close()
             assert False
 
 
-    def Test_Login(self, chrome_driver_init):
+    def test_DashboardPage(self, setup):
 
-        self.logger.info("Test_001_Home Page Title Verified")
-        self.driver = chrome_driver_init
+        self.logger.info("***** START Test 02: Dashboad Page title verification *****")
+        self.driver = setup
         self.driver.get(self.baseurl)
-        print("\nTest 2: Login and Dashboard title is compared\n")
+        print("\nTest 02: Login and Dashboard title is compared\n")
         self.lp = Login(self.driver)  #create of 'Login' class object
         self.lp.setUserName(self.username)
         self.lp.setPassword(self.password)
@@ -57,11 +53,12 @@ class Test_001_Login:
 
         if act_title == "Dashboard / nopCommerce administration":
             assert True
+            self.logger.info("***** Test 02 PASS: Dashboard Page title is expected *****")
             print( "Home Page Title is as expected")
             self.driver.close()
         else:
-            self.driver.save_screenshot(self.screenshotfolder+ '/Dashboard.png')
-            print("HomePage Title is not as expected")
+            self.driver.save_screenshot(self.ReadConfig.getDashboardscreenshot()+ '/Dashboard.png')
+            print("Test 02 FAIL: Dashboard Title is not as expected")
             self.driver.close()
             assert False
 
@@ -70,6 +67,16 @@ class Test_001_Login:
 
 
 """"
+Run One : pytest -s -v testCases/test_Login.py --browser chrome 
+Run parallel :pytest -v -n=2 testCases/test_Login.py 
+Run  :for HTML reports pytest -v -n=2 --html=Reports/report.html testCases/test_Login.py --browser chrome 
+
+
+
+ss_loginpage = "../Screenshots/Login.png"
+ss_dash = "..\\Screenshots\\" + "dashboard.png"
+
+
 - Sleep function use for hold execution on page for second that given in parameter
 from time import sleep
 sleep(2)
--- utilities/customeLogger.py
+++ utilities/customeLogger.py
@@ -5,9 +5,13 @@ class LogGenclass():
    def loggenmethod():
        for handler in logging.root.handlers[:]:
            logging.root.removeHandler(handler)
-           logging.basicConfig(filename='../Logs/automation.log', format='%(asctime)s %(message)s',datefmt='%m/%d/%Y %I:%M:%S %p',level=logging.INFO)
+           logging.basicConfig(filename=r'C:\Users\achal\PycharmProjects\nopcommerceApp\Logs\automation.log', format='%(asctime)s %(message)s',datefmt='%m/%d/%Y %I:%M:%S %p',level=logging.INFO)
            logger = logging.getLogger()
+           logger.setLevel(logging.INFO)
        return logger
 
+
+
+#filename='../Logs/automation.log'
 #call 'loggenmethod' method from 'LoGgenClass' class- return logger object
 #logger.setLevel(logging.INFO) //-if not mentioned in basicconfig write by setlevel()
